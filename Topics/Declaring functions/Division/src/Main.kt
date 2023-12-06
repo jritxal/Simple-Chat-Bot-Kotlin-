b86dcd9c525c0203fc315ccd877d0c11588abbdf
jritxal <javicionax@hotmail.com>
2023-12-06 21:44:58 +0100

    problem solved

---
+++ Topics/Declaring functions/Division/src/Main.kt
@@ -0,0 +1,9 @@
+// write your code here
+fun divide(a: Long, b: Long) = a / b.toDouble()
+
+/* Do not change code below */
+fun main() {
+    val a = readLine()!!.toLong()
+    val b = readLine()!!.toLong()
+    println(divide(a, b))
+}
